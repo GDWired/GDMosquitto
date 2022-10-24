ba356a78206d2b2439a5db15bfaedb476f4bb561
Jean-François Erdelyi <jferdelyi@gmail.com>
2022-10-24 19:25:41 +0200

    [QA] Change addon folder name to GDMosquitto

--- demo/addons/GDMosquitto/GDMosquitto.gd
+++ demo/addons/GDMosquitto/GDMosquitto.gd
@@ -8,7 +8,7 @@ extends EditorPlugin
 
 func _enter_tree():
 	add_custom_type("MQTTClient", "Node", preload("GDMosquitto.gdns"), preload("icon.png"))
-	add_autoload_singleton("GDMosquitto", "res://addons/mosquitto/GDMosquittoUtils.gd")
+	add_autoload_singleton("GDMosquitto", "res://addons/GDMosquitto/GDMosquittoUtils.gd")
 
 func _exit_tree():
 	remove_custom_type("MQTTClient")
